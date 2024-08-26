83bcb684759c3b6a7bda1109e1e94576180d709c
PRAVEEN887 <praveendhamodharncs2020@gmail.com>
2024-08-26 14:01:19 +0530

    .

--- QR-Code/src/Test.jsx
+++ QR-Code/src/Test.jsx
@@ -1,16 +1,31 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
+import './Test.css'
 
 export const Test = () => {
-    useEffect(()=>{
-        try{
-            axios.get("http://localhost:8080/students")
-            .then((res)=>console.log(res))
-        }catch(error){
-            console.log("Errors Found "+error);
-        }
-    })
+  const[count,setCount]=useState(0);
+  const click=(e)=>{
+      setCount(count+e);
+  }
+  const Nclick=(e)=>{
+    setCount(count-e);
+  }
+  const Reset=()=>{
+    setCount(0);
+  }
   return (
-    <div>Test</div>
+    <>
+      <div className='btn'>
+          <button onClick={()=>click(1)}>+1</button>
+          <button onClick={()=>click(2)}>+2</button>
+          <button onClick={()=>click(5)}>+5</button>
+          <button onClick={()=>Nclick(1)}>-1</button>
+          <button onClick={()=>Nclick(2)}>-2</button>
+          <button onClick={()=>Nclick(5)}>-5</button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={Reset}>Reset</button>
+      </div>
+    </>
   )
 }
